ad760b4949eb15525e3e0dc2ccf1ab58bee4e64c
Victor2005-18 <mvictormanuel633@gmail.com>
2026-06-15 09:54:23 -0600

    Subida oficial de la version 2 (#7)

---
+++ version 2/GestionEmpleadosHoteles/GestionEmpleadosHoteles/FormDetalle.Designer.vb
@@ -0,0 +1,188 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormDetalle
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        GroupBox1 = New GroupBox()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        Label7 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(253, 45)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(212, 25)
+        Label1.TabIndex = 0
+        Label1.Text = "DETALLES DE EMPLEADO"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(69, 132)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(187, 25)
+        Label2.TabIndex = 1
+        Label2.Text = "Informacion completa"
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Location = New Point(69, 179)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(530, 143)
+        GroupBox1.TabIndex = 2
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "GroupBox1"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(80, 358)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(115, 25)
+        Label3.TabIndex = 3
+        Label3.Text = "Salario Base: "
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(80, 420)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(91, 25)
+        Label4.TabIndex = 4
+        Label4.Text = "Bonos %: "
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(80, 480)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(111, 25)
+        Label5.TabIndex = 5
+        Label5.Text = "Horas Extra: "
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(80, 541)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(205, 25)
+        Label6.TabIndex = 6
+        Label6.Text = "Pago Mensual Calculado"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(143, 655)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(112, 34)
+        Button1.TabIndex = 7
+        Button1.Text = "Guardar"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(382, 655)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(112, 34)
+        Button2.TabIndex = 8
+        Button2.Text = "Cancelar"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(387, 541)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(22, 25)
+        Label7.TabIndex = 9
+        Label7.Text = "$"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(387, 358)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(150, 31)
+        TextBox1.TabIndex = 10
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(387, 420)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(150, 31)
+        TextBox2.TabIndex = 11
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(387, 480)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(150, 31)
+        TextBox3.TabIndex = 12
+        ' 
+        ' FormDetalle
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(665, 790)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label7)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(GroupBox1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "FormDetalle"
+        Text = "6"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+End Class
